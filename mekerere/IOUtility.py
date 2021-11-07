ccfca0266dd7c50d2624bbf0e9cf431aa43ecaa3
Jonathen <jonathensundy@gmail.com>
2021-11-07 15:08:07 +0200

    preprocess - crop and resize images
    Crop and resize images based on the training data bounding boxes. This will remove the noise from the existing training data by removing the background objects that are not relevant. This includes the background greenery as well other passion fruits in the image.

---
+++ mekerere/IOUtility.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+
+def crop_and_resize_on_bounding_box(filename, x_coord, y_coord, height, width):
+    image = cv.imread(filename)
+    cropped_image = image[y_coord:y_coord + height, x_coord:x_coord + width]
+    width = int(image.shape[1])
+    height = int(image.shape[0])
+    dimension = (width, height)
+    resized = cv.resize(cropped_image, dimension, interpolation=cv.INTER_AREA)
+    return resized
+
+
+def write_image_to_file(filename, image):
+    cv.imwrite(filename, image)
